d9314a81bc411a7b3bb7e0c8bb21062a7c5829ff
Branden Langhals <58713197+Brandenlanghals@users.noreply.github.com>
2023-08-01 10:04:26 -0400

    made progress with the module that I cannot lose again

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const hbs = require("hbs");
+const passport = require("passport");
+
 require("./app_api/database/db");
+
+require("./app_api/config/passport");
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
 var travelRouter = require("./app_server/routes/travel");
+var roomsRouter = require("./app_server/routes/rooms");
+var newsRouter = require("./app_server/routes/news");
+var mealsRouter = require("./app_server/routes/meals");
 var contactRouter = require("./app_server/routes/contact");
 var aboutRouter = require("./app_server/routes/about");
 
-const apiRouter = require("./app_api/routes/index"); // Include the API router
+const apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
@@ -27,13 +36,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
 
 // Allow CORS
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
@@ -42,10 +52,19 @@ app.use("/api", (req, res, next) => {
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/travel", travelRouter);
+app.use("/rooms", roomsRouter);
+app.use("/news", newsRouter);
+app.use("/meals", mealsRouter);
 app.use("/contact", contactRouter);
 app.use("/about", aboutRouter);
 
-app.use("/api", apiRouter); // Mount the API routes at the /api endpoint
+app.use("/api", apiRouter);
+
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+
+const jwt = require("express-jwt");
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: "payload",
+  algorithms: ["HS256"],
+});
+
+const authController = require("../controllers/authentication");
 const tripsController = require("../controllers/trips");
 
-// Route to get all trips
-router.route("/trips").get(tripsController.tripsList);
+router.route("/login").post(authController.login);
 
-// Route to get a single trip by tripCode
+router.route("/register").post(authController.register);
+
+router
+  .route("/trips")
+  .get(tripsController.tripsList)
+  .post(auth, tripsController.tripsAddTrip);
+
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsList)
+  .put(auth, tripsController.tripsUpdateTrip);
+
 router.route("/trips/:tripCode").get(tripsController.tripsFindCode);
 
 module.exports = router;
